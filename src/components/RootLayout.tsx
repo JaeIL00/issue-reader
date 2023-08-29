5518e5639482547e7a3c4e73becb29778dc3b04a
ShinJaeIl01 <ekvm555@naver.com>
2023-08-29 20:08:53 +0900

    design: 글로벌 헤더 스타일링

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 const RootLayout = () => {
     return (
-        <main>
-            <Outlet />
-        </main>
+        <GlobalContainer>
+            <GlobalHeader>
+                <h1>React Issues</h1>
+            </GlobalHeader>
+            <main>
+                <Outlet />
+            </main>
+        </GlobalContainer>
     );
 };
+
+const GlobalContainer = styled.div`
+    padding-top: 8vh;
+    max-width: 40vw;
+    margin: 0 auto;
+`;
+const GlobalHeader = styled.header`
+    display: flex;
+    justify-content: center;
+    font-size: 2.4rem;
+`;
 
 export default RootLayout;
