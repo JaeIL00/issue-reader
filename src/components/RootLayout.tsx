9f8e41254c59529d3d3219f2f19783370682a11f
ShinJaeIl01 <ekvm555@naver.com>
2023-08-30 11:18:46 +0900

    design: Global Style 수정

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -7,22 +7,29 @@ const RootLayout = () => {
             <GlobalHeader>
                 <h1>React Issues</h1>
             </GlobalHeader>
-            <main>
+            <Main>
                 <Outlet />
-            </main>
+            </Main>
         </GlobalContainer>
     );
 };
 
 const GlobalContainer = styled.div`
-    padding-top: 8vh;
+    background-color: #fff;
     max-width: 70vw;
     margin: 0 auto;
 `;
 const GlobalHeader = styled.header`
-    display: flex;
-    justify-content: center;
+    width: 72vw;
+    padding-top: 8vh;
+    margin-left: -1vw;
     font-size: 2.4rem;
+    position: fixed;
+    text-align: center;
+    background-color: #fff;
+`;
+const Main = styled.main`
+    padding-top: 15.6vh;
 `;
 
 export default RootLayout;
