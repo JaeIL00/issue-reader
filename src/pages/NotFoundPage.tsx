7e430b44c863f3d4f4d3b58dfb8a2ca2438de54d
ShinJaeIl01 <ekvm555@naver.com>
2023-08-30 18:43:59 +0900

    feat: Not Found Page 버튼 추가

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFoundPage = () => {
-    return <span>잘못된 요청입니다</span>;
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <span>잘못된 요청입니다</span>
+            <br />
+            <button onClick={() => navigate("/")}>이슈 목록으로 이동</button>
+        </>
+    );
 };
 
 export default NotFoundPage;
